3bd954b277be4e6da157dbd3dbcb3cd7fe8a68ab
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-24 11:25:15 +0530

    new

---
+++ script.js
@@ -0,0 +1,41 @@
+// let firstName ="Bishnu"
+// let lastName = "Sharma"
+// let videos = 30
+// let subscriber = 1000
+
+// let fullName = firstName +" "+ lastName
+
+// console.log(fullName)
+
+// console.log(firstName[0])
+// console.log(firstName[1])
+// console.log(firstName[2])
+
+// //length of first name
+// console.log(firstName.length)
+// //convert to upper case
+// console.log(firstName.toUpperCase())
+
+// console.log(firstName.startsWith("V"))
+// console.log(lastName.endsWith("a"))
+
+// console.log(fullName.includes("ar"))
+
+// let htmlTemplate = `<h1>${firstName}${lastName}</h1>
+// <p>Videos: ${videos}</p>
+// <p>Subscribers: ${subscriber}</p>`
+
+// console.log(htmlTemplate)
+
+let product = ["Roti", "Sabji", "Paani Puri", "Sab khao"]
+let update  = ["Bread", "v kha le"]
+let productsMethod = product.join(",")
+console.log(productsMethod)
+console.log(product)
+
+let productsMethod1 = product.indexOf("Roti")
+console.log(productsMethod1)
+console.log(product)
+
+let productsMethod2 = product.concat(update)
+console.log(productsMethod2)
