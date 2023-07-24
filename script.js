16507c51e952133073f01170da0fde85a801924b
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-24 11:27:06 +0530

    update

--- script.js
+++ script.js
@@ -27,15 +27,32 @@
 
 // console.log(htmlTemplate)
 
-let product = ["Roti", "Sabji", "Paani Puri", "Sab khao"]
-let update  = ["Bread", "v kha le"]
-let productsMethod = product.join(",")
-console.log(productsMethod)
-console.log(product)
+// let product = ["Roti", "Sabji", "Paani Puri", "Sab khao"]
+// let update  = ["Bread", "v kha le"]
+// let productsMethod = product.join(",")
+// console.log(productsMethod)
+// console.log(product)
 
-let productsMethod1 = product.indexOf("Roti")
-console.log(productsMethod1)
-console.log(product)
+// let productsMethod1 = product.indexOf("Roti")
+// console.log(productsMethod1)
+// console.log(product)
 
-let productsMethod2 = product.concat(update)
-console.log(productsMethod2)
+// let productsMethod2 = product.concat(update)
+// console.log(productsMethod2)
+
+
+// let productsMethod3 = product.push("water v pio")
+// console.log(productsMethod3)
+// console.log(product)
+
+
+let score = 20;
+console.log(score)
+
+let update  = null
+console.log(update)
+console.log(update, update+20, `My score is ${update}`)
+
+let year
+console.log(year)
+console.log(year, year + 20, `My year is ${year}`)
